6890b07bd9285dd12abcf42a6417bac89a8d9e29
sush19mita <144100867+sush19mita@users.noreply.github.com>
2026-07-04 22:15:10 +0530

    Fix typo in algorithm and model names

--- app.py
+++ app.py
@@ -48,8 +48,8 @@ with tab1:
         'ST_Slope': [st_slope_dict]
     })
 
-    algoNames = ["Logistic Regression", "Support Vector Machine", "Random Forest", "XG Boost Classifier", "BEACONX_VWDMEC"]
-    modelNames = ["LogisticR .pickle", "SVM.pickle", "RFC1.pickle", "XGB.pickle", "SVM.pkl"]
+    algoNames = ["Logistic Regression", "Support Vector Machine", "XG Boost Classifier", "BAECONX_VWDMEC"]
+    modelNames = ["LogisticR .pickle", "SVM.pickle",  "XGB.pickle", "RFC1.pickle"]
 
     # Function to make predictions
     def predict_heart_disease(data):
